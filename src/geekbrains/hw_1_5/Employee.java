db57d85628d2778f9eac9446ebb46827bb139f1b
Chobotock <ilya_ageev@bk.ru>
2020-08-02 20:23:16 +0300

    hw_1_5

--- src/geekbrains/hw_1_5/Employee.java
+++ src/geekbrains/hw_1_5/Employee.java
@@ -1,5 +1,24 @@
 package geekbrains.hw_1_5;
 
-public class Employee {
-    
+public class Employee {             // Создан класс "Сотрудник";
+
+    String fullName;                // Созданы требуемые поля;
+    String position;
+    String eMail;
+    String phone;
+    int salary;
+    int age;
+
+    public Employee(String fullName, String position, String eMail, String phone, int salary, int age) {    // Создан конструктор через Alt + Insert;
+        this.fullName = fullName;
+        this.position = position;
+        this.eMail = eMail;
+        this.phone = phone;
+        this.salary = salary;
+        this.age = age;
+    }
+
+    public void info() {            // Внутри класса "Сотрудник" создан метод, который выводит информацию об объекте в консоль;
+        System.out.printf(" fullName %s%n position %s%n eMail %s%n phone %s%n salary %s%n age %d %n%n", fullName, position, eMail, phone, salary, age);
+    }
 }
--- src/geekbrains/hw_1_5/Main.java
+++ src/geekbrains/hw_1_5/Main.java
@@ -4,7 +4,33 @@ public class Main {
 
     public static void main(String[] args) {
 
+//    Employee employee1 = new Employee();              // Здесь пробовал без конструктора;
+//
+//    employee1.fullName = "Ivanov Ivan Ivanovich";
+//    employee1.position = "Engineer";
+//    employee1.eMail = "Ivanych@mail.ru";
+//    employee1.phone = "89123456789";
+//    employee1.salary = 90000;
+//    employee1.age = 27;
 
+    Employee employee2 = new Employee("Petrov Petr Petrovich", "Senior engineer", "Petrovich@gmail.com", "89187654321", 130000, 86);
 
+//    employee1.info();                                 // Печать без конструктора;
+    employee2.info();
+
+    Employee[] employees = new Employee[5];             // Создан массив из 5 сотрудников;
+    employees[0] = new Employee("Petrov Petr Petrovich", "Senior engineer", "Petrovich@gmail.com", "89187654321", 130000, 48);
+    employees[1] = new Employee("Ivanov Ivan Ivanovich", "Engineer", "Ivanych@mail.ru", "89123456789", 90000, 27);
+    employees[2] = new Employee("Ilyin Ilya Ilyich", "Programmer", "Ilyich@gmail.com", "89187853721", 150000, 32);
+    employees[3] = new Employee("Mishin Mikhail Mikhailovich", "Economist", "Mikhailovich@bk.ru", "89582654329", 180000, 41);
+    employees[4] = new Employee("Stepanov Stepan Stepanovich", "Director", "Stepanovich@gmail.com", "89198417321", 250000, 32);
+
+    System.out.println("*** Employees over 40 years of age: ***\n");
+
+        for (int i = 0; i < employees.length ; i++) {   // С помощью цикла выводим информацию только о сотрудниках старше 40 лет;
+            if (employees[i].age > 40) {
+                employees[i].info();
+            }
+        }
     }
 }
