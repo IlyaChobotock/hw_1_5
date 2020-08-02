5aa5ac1c0c4517e58477e04be440d94fee26061b
Chobotock <ilya_ageev@bk.ru>
2020-08-02 18:30:20 +0300

    Initial commit

---
+++ src/geekbrains/hw_1_5/Employee.java
@@ -0,0 +1,5 @@
+package geekbrains.hw_1_5;
+
+public class Employee {
+    
+}
---
+++ src/geekbrains/hw_1_5/Main.java
@@ -0,0 +1,10 @@
+package geekbrains.hw_1_5;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+
+    }
+}
